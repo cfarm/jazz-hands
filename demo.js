2fc5a1a504c59f6f55126872566e710c334adbcc
Catherine Farman <Catherine.Farman@cfpb.gov>
2015-03-07 18:21:24 -0500

    random gettin and settin

--- demo.js
+++ demo.js
@@ -1,13 +1,20 @@
 var Todo = Backbone.Model.extend({
   initialize: function() {
-    console.log("This model Todo has been initialized.")
+    console.log("This model Todo has been initialized.");
+    this.on('change:title', function() {
+      console.log(' - The Title for this module has changed');
+    });
+    this.on('change:completed', function() {
+      console.log(' - The completed status of this todo model has changed');
+    });
   },
   defaults: {
     title: '',
     completed: false
+  },
+  setTitle: function(newTitle){ this.set({ title: newTitle });
   }
 });
-
 
 // create our own instance of the Todo Model
 var toodles = new Todo({
@@ -19,7 +26,23 @@ var toodles2 = new Todo({
   completed: true
 });
 
-console.log(JSON.stringify(toodles));
-console.log(JSON.stringify(toodles2));
+var toodlesProperties = toodles.toJSON();
 
-console.log(toodles.get('title'));
+console.log(toodlesProperties);
+
+console.log('JSON.stringify: ' + JSON.stringify(toodles));
+// console.log(JSON.stringify(toodles2));
+
+// console.log(toodles.get('title'));
+
+
+// Set stuff
+
+toodles2.set("title", "A new title using Model.set()");
+
+toodles.set({
+  title: "a new title set with multiple attributes using set()",
+  completed: true
+});
+
+console.log(JSON.stringify(toodles))
